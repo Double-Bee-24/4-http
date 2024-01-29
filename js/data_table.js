7b0c32b4c2ac3675f5ee287d6a23cf92c6ac3ab7
Bohdan Bilokin <bodya24102001@gmail.com>
2024-01-29 20:44:07 +0200

    Small fixes

--- js/data_table.js
+++ js/data_table.js
@@ -1,38 +1,53 @@
-import {sendData, getServerData, changeData, deleteItem} from "./serverRequests.js";
+import { sendData, getServerData, changeData, deleteItem } from "./server_requests.js";
 
 export async function DataTable(config) {
     const tableDiv = document.querySelector(config.parent);
     tableDiv.classList.add("parent")
     const table = document.createElement("table");
 
-    createAddButton(tableDiv, config);
+    const data = await getServerData(config);
+    createAddButton(tableDiv, config, data);
     tableDiv.appendChild(table);
     table.classList.add("table");
 
-    const data = await getServerData(config);
-
     createTableHead(table, config);
-    createTableBody(table, config, data);
+    createTableBody(table, config, data, tableDiv);
 }
 
-function createAddButton(tableDiv, config) {
+function createAddButton(tableDiv, config, data) {
     const addButton = document.createElement("button");
     addButton.innerText = "Додати";
     addButton.classList.add("add-button");
     tableDiv.appendChild(addButton);
-    addButton.addEventListener("click", () => addInputRow(tableDiv, config, 0));
+    addButton.addEventListener("click", () => addInputRow(tableDiv, config, data));
 }
 
-function addInputRow(tableDiv, config, index) {
+function addInputRow(tableDiv, config, data, tableRow = 0) {
     const tbody = tableDiv.querySelector("tbody");
+    const table = tableDiv.querySelector("table");
 
-    if (tableDiv.querySelector(".input-row") === null) {
-        const inputRow = tbody.insertRow(index);
+    let rowIndex;
+    let actualUser;
+
+    if (typeof tableRow === "number") {
+        rowIndex = tableRow;
+    } else {
+        rowIndex = tableRow.querySelector("td").innerText; // take a number from number column
+        actualUser = data.data[tableRow.id];
+    }
+
+    let inputRow = tableDiv.querySelector(".input-row");
+
+    if (inputRow === null) {
+        // tableRow.classList.add("hidden");
+
+        inputRow = tbody.insertRow(rowIndex);
         inputRow.classList.add("input-row");
+        // const actualObject = tableRow.id;
 
         const columns = config.columns;
         const numberCell = document.createElement("td");
-        numberCell.innerText = "№";
+        numberCell.innerText = rowIndex || "№";
         inputRow.append(numberCell);
 
         for (let column of columns) {
@@ -64,8 +79,17 @@ function addInputRow(tableDiv, config, index) {
                     } else {
                         // Create input element
                         const inputElement = document.createElement("input");
-                        inputElement.addEventListener("keydown", () => sendData(event, tbody, config));
                         inputElement.addEventListener("input", () => setDefaultFrame(inputElement));
+
+                        if (actualUser) {
+                            inputElement.value = actualUser[arrayElement.name];
+                            inputElement.addEventListener("keydown",
+                                () => changeData(event, table, config, inputRow, tableRow.id, tableDiv));
+                        } else {
+                            inputElement.addEventListener("keydown",
+                                () => sendData(event, tbody, config, tableDiv));
+                        }
+
                         for (const inputValue in arrayElement) {
                             inputElement[inputValue] = arrayElement[inputValue];
                         }
@@ -74,8 +98,22 @@ function addInputRow(tableDiv, config, index) {
                 }
             } else { // If we have only one input - it is an object in configurations
                 const inputElement = document.createElement("input");
-                inputElement.addEventListener("keydown", () => sendData(event, tbody, config));
                 inputElement.addEventListener("input", () => setDefaultFrame(inputElement));
+
+                if (actualUser) { // If we need to edit an existing row
+                    inputElement.value = actualUser[column.value] || actualUser[column.input.name];
+                    if (inputElement.type === "date") {
+                        const date = actualUser[column.input.name].split("T")[0];
+                        inputElement.value = date;
+                    }
+                    inputElement.addEventListener("keydown",
+                        () => changeData(event, table, config, inputRow, tableRow.id, tableDiv));
+                } else {
+                    inputElement.value = "";
+                    inputElement.addEventListener("keydown",
+                        () => sendData(event, tbody, config, tableDiv));
+                }
+
                 for (const inputValue in columnInput) {
                     inputElement[inputValue] = columnInput[inputValue];
                 }
@@ -84,6 +122,9 @@ function addInputRow(tableDiv, config, index) {
 
             inputRow.append(cell);
         }
+    } else {
+        inputRow.remove();
+        // tableRow.classList.remove("hidden");
     }
 }
 
@@ -113,7 +154,7 @@ function createTableHead(table, config) {
     titleRow.appendChild(actionCell);
 }
 
-export function createTableBody(table, config, data) { // Create rows for table body
+export function createTableBody(table, config, data, tableDiv) { // Create rows for table body
     if (table.querySelector("tbody") !== null) {
         table.querySelector("tbody").remove();
     }
@@ -124,10 +165,11 @@ export function createTableBody(table, config, data) { // Create rows for table 
     // Create row
     let rowNumber = 1;
     for (let row in allUsersList) {
- 
+
         const user = allUsersList[row];
         // Create number cell
         const tableRow = tbody.insertRow(rowNumber - 1);
+        tableRow.id = row;
         const numberTd = document.createElement("td");
         numberTd.innerText = rowNumber
         tableRow.appendChild(numberTd);
@@ -165,101 +207,16 @@ export function createTableBody(table, config, data) { // Create rows for table 
         deleteButton.classList.add("delete-button");
         actionCell.append(deleteButton);
         deleteButton.innerText = "Видалити";
-        deleteButton.onclick = function () { deleteItem(row, table, config) }
+        deleteButton.onclick = function () { deleteItem(row, table, config, tableDiv) }
 
         const editButton = document.createElement("button");
         editButton.classList.add("delete-button");
         editButton.classList.add("edit-button");
         actionCell.append(editButton);
         editButton.innerText = "Редагувати";
-        editButton.addEventListener("click", () => setInputRow(tableRow, config, allUsersList, table, editButton));
+        // editButton.addEventListener("click", () => setInputRow(tableRow, config, allUsersList, table, editButton));
+        editButton.addEventListener("click", () => addInputRow(tableDiv, config, data, tableRow));
 
         tableRow.appendChild(actionCell);
     }
-}
-
-function setInputRow(row, config, allUsersList, table) {
-    const cells = row.querySelectorAll("td");
-    
-    const rowNumber = cells[0].innerText;
-    let objectIndex = 1;
-    let actualObject;
-    let actualObjectId;
-
-    for(const key in allUsersList){
-        if(objectIndex === +rowNumber){
-            actualObject = allUsersList[key];
-            actualObjectId = key;
-            break;
-        }
-        objectIndex++;
-    }
-
-    if (row.querySelector("input") === null) {
-
-        const columns = config.columns;
-
-        let cellIndex = 1;
-
-        for (let i = 0; i < columns.length; i++) {
-            let column = columns[i];
-            const columnInput = column.input;
-            let cell = cells[cellIndex++];
-
-            // If we have more than one input - it is an array with objects in configurations
-            if (Array.isArray(columnInput)) {
-                for (let arrayElement of columnInput) {
-                    // Create select element
-                    if (arrayElement.type === "select") {
-                        const selectElement = document.createElement("select");
-                        for (const inputValue in arrayElement) {
-                            if (inputValue === "options") {
-                                const options = arrayElement[inputValue];
-
-                                for (const option of options) {
-                                    const optionElement = document.createElement("option");
-                                    optionElement.innerText = option;
-                                    selectElement.appendChild(optionElement);
-                                }
-                            } else {
-                                selectElement.setAttribute(inputValue, arrayElement[inputValue]);
-                            }
-                        }
-
-                        cell.appendChild(selectElement);
-                    } else {
-                        // Create input element
-                        const inputElement = document.createElement("input");
-                        inputElement.value = actualObject[arrayElement.name];
-                        inputElement.type = arrayElement.type;
-
-                        
-                        // alert(row);
-                        inputElement.addEventListener("keydown", () => changeData(event, table, config, row, actualObjectId));
-                        inputElement.addEventListener("input", () => setDefaultFrame(inputElement));
-                        cell.innerText = "";
-                        cell.appendChild(inputElement);
-                    }
-                }
-            } else { // If we have only one input - it is an object in configurations
-                const inputElement = document.createElement("input");
-                // alert(row);
-
-                inputElement.addEventListener("keydown", () => changeData(event, table, config, row, actualObjectId));
-                inputElement.addEventListener("input", () => setDefaultFrame(inputElement));
-
-                for (const inputValue in columnInput) {
-                    inputElement[inputValue] = columnInput[inputValue];
-                    inputElement.value = actualObject[column.value]  || actualObject[column.input.name];
-                    if(inputElement.type === "date"){
-                        const date = actualObject[column.input.name].split("T")[0];
-                        inputElement.value = date;
-                    }
-                }
-                cell.innerText = "";
-                cell.appendChild(inputElement);
-            }
-
-        }
-    } 
 }
